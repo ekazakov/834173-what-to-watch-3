ba5ae021f6f811d3eb1b9c4a6383f12010d9fe1e
Dana <kovach.dana@yandex.ru>
2020-02-07 14:55:44 +0300

    заменила магическое 8 на длину массива с заголовками в main.e2e.test

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -29,5 +29,5 @@ it(`Should title of movie be pressed`, () => {
     title.props().onClick();
   });
 
-  expect(onTitleOfMovieClick.mock.calls.length).toBe(8);
+  expect(onTitleOfMovieClick.mock.calls.length).toBe(titlesOfMovie.length);
 });
