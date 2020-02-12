a5b10089dccb8e1834dee1c8ad2514e87b8b3bb0
Dana <kovach.dana@yandex.ru>
2020-02-12 11:35:37 +0300

    моки для фильмов

---
+++ src/mocks/films.js
@@ -0,0 +1,29 @@
+const RANDOM_IMAGE = `https://unsplash.it/280/175/?random`;
+
+export default [
+  {
+    name: `Harry Potter and the Goblet of Fire`,
+    genre: `drama`,
+    src: `${RANDOM_IMAGE}`,
+  },
+  {
+    name: `EuroTrip`,
+    genre: `comedy`,
+    src: `${RANDOM_IMAGE}`,
+  },
+  {
+    name: `The Autopsy of Jane Doe`,
+    genre: `thriller`,
+    src: `${RANDOM_IMAGE}`,
+  },
+  {
+    name: `The Notebook`,
+    genre: `romance`,
+    src: `${RANDOM_IMAGE}`,
+  },
+  {
+    name: `Carri`,
+    genre: `horror`,
+    src: `${RANDOM_IMAGE}`,
+  },
+];
