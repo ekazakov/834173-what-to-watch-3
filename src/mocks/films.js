299c83249acdb638df09d93dd3616f73c3ef0f33
Dana <kovach.dana@yandex.ru>
2020-02-18 22:57:44 +0300

    добавила мок превью

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,4 +1,6 @@
 const RANDOM_IMAGE = `https://unsplash.it/280/175/?random`;
+const PREVIEW_VIDEO_FIRST = `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`;
+const PREVIEW_VIDEO_SECOND = `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`;
 
 export default [
   {
@@ -6,29 +8,34 @@ export default [
     name: `Harry Potter and the Goblet of Fire`,
     genre: `drama`,
     poster: `${RANDOM_IMAGE}`,
+    preview: `${PREVIEW_VIDEO_FIRST}`
   },
   {
     id: 2,
     name: `EuroTrip`,
     genre: `comedy`,
     poster: `${RANDOM_IMAGE}`,
+    preview: `${PREVIEW_VIDEO_SECOND}`
   },
   {
     id: 3,
     name: `The Autopsy of Jane Doe`,
     genre: `thriller`,
     poster: `${RANDOM_IMAGE}`,
+    preview: `${PREVIEW_VIDEO_FIRST}`
   },
   {
     id: 4,
     name: `The Notebook`,
     genre: `romance`,
     poster: `${RANDOM_IMAGE}`,
+    preview: `${PREVIEW_VIDEO_SECOND}`
   },
   {
     id: 5,
     name: `Carri`,
     genre: `horror`,
     poster: `${RANDOM_IMAGE}`,
+    preview: `${PREVIEW_VIDEO_FIRST}`
   },
 ];
