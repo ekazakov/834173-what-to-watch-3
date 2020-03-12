cee871992f23940a0f3fc2dc4223ab139b472cdb
Dana <kovach.dana@yandex.ru>
2020-03-12 14:32:55 +0300

    подключила user в reducer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,8 +2,10 @@ import NameSpace from "./name-space.js";
 import {combineReducers} from "redux";
 import {reducer as state} from "./state/state.js";
 import {reducer as data} from "./data/data.js";
+import {reducer as user} from "./user/user.js";
 
 export default combineReducers({
   [NameSpace.STATE]: state,
   [NameSpace.DATA]: data,
+  [NameSpace.USER]: user,
 });
